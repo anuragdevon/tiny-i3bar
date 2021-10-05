669bf1749326b6ff832d697aae6520d6c3e5eab2
anuragdevon <msm19b021@iiitdm.ac.in>
2021-10-06 01:27:46 +0530

    add new functionalities, fix race cond.

--- mybar/mybar.sh
+++ mybar/mybar.sh
@@ -7,9 +7,19 @@ echo '[]'
 while :;
 do
   echo -n ",["
-  echo -n "{\"name\":\"id_battery\",\"background\":\"#000000\",\"full_text\":\"$(battery)%\"},"
-  echo -n "{\"name\":\"id_cpu\",\"background\":\"#000000\",\"full_text\":\"$(/home/anurag/.local/bin/scripts/mybar/cpu_load)%\"},"
-  echo -n "{\"name\":\"id_time\",\"background\":\"#000000\",\"full_text\":\"$(/home/anurag/.local/bin/scripts/mybar/date_time)\"}"
+  # Sections
+  # Battery
+  echo -n "{\"name\":\"id_battery\",\"background\":\"#000000\",\"full_text\":\"$(~/.local/bin/scripts/mybar/wifi) \"},"
+  # CPU Load
+  echo -n "{\"name\":\"id_cpu\",\"background\":\"#000000\",\"full_text\":\"$(~/.local/bin/scripts/mybar/cpu_load)%\"},"
+  # Memory
+  echo -n "{\"name\":\"id_battery\",\"background\":\"#000000\",\"full_text\":\"$(~/.local/bin/scripts/mybar/memory)\"},"
+  # Disk Uage
+  echo -n "{\"name\":\"id_battery\",\"background\":\"#000000\",\"full_text\":\"$(~/.local/bin/scripts/mybar/disk)\"},"
+  # Battery
+  echo -n "{\"name\":\"id_battery\",\"background\":\"#000000\",\"full_text\":\"$(~/.local/bin/scripts/mybar/bat)\"},"
+  # Date_Time
+  echo -n "{\"name\":\"id_time\",\"background\":\"#000000\",\"full_text\":\"$(~/.local/bin/scripts/mybar/date_time)\"}"
   echo -n "]"
   sleep 1
 done
